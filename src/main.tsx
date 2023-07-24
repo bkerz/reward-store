7ce496a690c86471672ded003b754c3640dabb1b
Beicker Zambrano <beicker998@gmail.com>
2023-07-24 17:13:31 -0500

    Add settings page

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,17 @@ import {
 	RouterProvider,
 } from "react-router-dom";
 import App from './App.tsx';
+import Settings from './settings/Settings.tsx';
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <App />
 	},
+	{
+		path: "/settings",
+		element: <Settings />
+	}
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
